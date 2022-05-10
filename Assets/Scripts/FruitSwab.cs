22ad681e2a296959298b70038f6c3ad4764336ac
OsmanX5 <Gamemakerx5@gmail.com>
2022-05-10 21:37:37 +0200

    fruit swab cleaning

--- Assets/Scripts/FruitSwab.cs
+++ Assets/Scripts/FruitSwab.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class FruitSwab : MonoBehaviour
 {
-    public bool canSwab = true;
+    // Script controling in swaping 
+    // attach to Game manger
+    // Methdology:  1. check if I player hovering over and fruit and set the <HoveringObject> Object for what the player hovering
+    //              2. if pressed left click make the hovering object <SelectedObject> and take a list of all arounds fruit for the hovering
+    //              3. if the mouse move to hover an object from the upper list make the swab
+    //              4. else return the <SelectedObject> to null;
+    public bool isSwaping = true;
     public static GameObject HoveringObject = null;
     public static GameObject SelectedObject = null;
-    GameObject toSwab;
+    public GameObject toSwab;
     public LayerMask layer;
-    bool startDraging = false;
-    bool endDraging = false;
-    GameManger manger; 
+    GameManger manger;
+    public GameObject[] FruitsArroundSelectedObject = new GameObject[4] ; // List contain refrences for the gameobjects arround the selected object
+    bool playerIsSelected = false; // bool variable viewing if the player selected an object 
     private void Awake()
     {
         manger = this.GetComponent<GameManger>();
     }
     private void FixedUpdate()
     {
+       // 1.check if I player hovering over and fruit and set the<HoveringObject> Object for what the player hovering
         RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero,100, layer);
         if(hit.collider != null)
         {
@@ -26,33 +33,54 @@ public class FruitSwab : MonoBehaviour
     }
     private void Update()
     {
-        
-        if (HoveringObject != null && endDraging==false)
+        // if the player hovering Over an object
+        if (HoveringObject != null )
         {
             HoveringObject.GetComponent<SpriteRenderer>().color = new Color(180,180,180,0.5f);
-            if (Input.GetMouseButtonDown(0) && !startDraging) {
-                startDraging = true;
-                SelectedObject = HoveringObject;
+            // 2. if pressed left click make the hovering object <SelectedObject> and take a list of all arounds fruit for the hovering
+            if (Input.GetMouseButton(0) ) {
+                if(!playerIsSelected)
+                {
+                    SelectedObject = HoveringObject;
+                    playerIsSelected = true;
+                    FruitsArroundSelectedObject = SelectedObject.GetComponent<item>().aroundFruitsObjects;
+                }
+                
             }
-            if(startDraging && HoveringObject!= SelectedObject)
+            else
             {
-                toSwab = HoveringObject;
-                endDraging = true;
-                swab(SelectedObject, toSwab);
+                
+                 SelectedObject = null;
+                playerIsSelected = false;
+                FruitsArroundSelectedObject = new GameObject[4];
+
+            }
+            if( HoveringObject!= SelectedObject && playerIsSelected)
+            {
+                foreach (GameObject fruit in FruitsArroundSelectedObject)
+                {
+                    if (fruit == HoveringObject)
+                    {
+                        toSwab = HoveringObject;
+                    }
+                }
+                SelectedObject = null;
+                playerIsSelected = false;
+                //swab(SelectedObject, toSwab);
                // Debug.Log(SelectedObject.name + "  " + toSwab.name);
             }
         }
         if (Input.GetMouseButtonUp(0)) { 
-            endDraging = false; 
-            startDraging = false;
             SelectedObject = null;
             toSwab = null;
         }
 
 
     }
-    void swab(GameObject firstObject,GameObject SecondObject)
+    IEnumerator swab()
     {
+        GameObject firstObject = SelectedObject;
+        GameObject SecondObject = toSwab;
         firstObject.GetComponent<BoxCollider2D>().enabled = false;
         SecondObject.GetComponent<BoxCollider2D>().enabled = false;
 
@@ -65,5 +93,6 @@ public class FruitSwab : MonoBehaviour
         manger.GetComponent<ScoreManger>().swabs -= 1;
         firstObject.GetComponent<item>().checkForMatches();
         SecondObject.GetComponent<item>().checkForMatches();
+        yield return null;
     }
 }
